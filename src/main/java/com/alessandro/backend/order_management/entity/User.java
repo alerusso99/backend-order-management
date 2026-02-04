05b0d2fbf058c79ab8cf1628b0a72723029e0814
Alessandro Russo <alessandrorusso601@gmail.com>
2026-02-04 17:12:08 +0100

    Add Entity User.java Add Repository UserRepository.java Update application.yaml

---
+++ src/main/java/com/alessandro/backend/order_management/entity/User.java
@@ -0,0 +1,47 @@
+package com.alessandro.backend.order_management.entity;
+
+import jakarta.persistence.Table;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+
+    @Column(nullable = false)
+    private String name;
+
+    protected User() {
+
+    }
+
+    public User(String email, String name) {
+        this.email = email;
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
